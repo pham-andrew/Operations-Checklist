1600131c3006a5db917c866136184316547b57fa
ac-cool-breeze <alexcolter@gmail.com>
2021-07-06 10:28:17 -0500

    initial db seed file created

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port,  ()=>{
     console.log(`Listening on ${port}`)
 })
--- backend/db.sql
+++ backend/db.sql
@@ -1,17 +1,46 @@
 CREATE DATABASE checklistApp;
 
+\c checklistApp
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username SERIAL VARCHAR(50),
     password VARCHAR(255),
-    group INT,
     admin BOOLEAN
 );
 
 CREATE TABLE checklists(
     id SERIAL PRIMARY KEY,
     author VARCHAR(50),
-    group INT,
-    todos VARCHAR()
-)
+    title VARCHAR(255),
+    archived BOOLEAN
+);
+
+CREATE TABLE todos(
+    id SERIAL PRIMARY KEY,
+    img BYTEA,
+    order INT,
+    completed BOOLEAN,
+    todo VARCHAR(255)
+);
+
+-- JOIN TABLES
+CREATE TABLE completed_lists(
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    checklist_id INT,
+    date_completed DATETIME
+);
+
+CREATE TABLE users_list(
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    checklist_id INT,
+    role VARCHAR(255)
+);
+
+CREATE TABLE todos_list(
+    id SERIAL PRIMARY KEY,
+    todos_id INT,
+    checklist_id INT
+);
