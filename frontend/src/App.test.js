aa00c339c8cd0a0478c188b160b884c35a10d56b
pham-andrew <andrewpham@g.harvard.edu>
2021-07-08 07:09:18 -0700

    failing routing test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import { createMemoryHistory } from 'history';
 
-test('renders learn react link', () => {
+
+test('renders home link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('renders login link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders create checklist link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Create a checklist/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('routes to a new route', async () => {
+  render(<App />);
+  const history = createMemoryHistory();
+  history.push = jest.fn();
+
+  const linkElement = screen.getByText(/Login/i);
+  fireEvent.click(linkElement);
+
+  expect(history.push).toHaveBeenCalledWith('/login');
 });
